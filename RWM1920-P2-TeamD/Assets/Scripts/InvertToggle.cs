6ff6b186c42f12855e68a403d4eae7027427434c
GregCahillC00216328 <gregcahill17@gmail.com>
2019-11-26 15:48:27 +0000

    Added and modified grav field for 2D use

---
+++ RWM1920-P2-TeamD/Assets/Scripts/InvertToggle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvertToggle : MonoBehaviour {
+
+    Vector2 centrePoint;
+    public GameObject Go;
+    GravInvert grav;
+    
+
+    void Start()
+    {
+        centrePoint = GetComponent<Renderer>().bounds.center;
+       
+        grav = Go.GetComponent<GravInvert>();
+        
+
+
+
+    }
+    
+    
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.tag==("Finish"))
+        {
+            //Debug.Log("col detected");
+            if (other.gameObject.transform.position.x <centrePoint.x)
+            {
+                GravInvert.invertVal = (other.gameObject.transform.position.x - centrePoint.x) ;
+            }
+            else if (other.gameObject.transform.position.x > centrePoint.x)
+            {
+                GravInvert.invertVal = ((other.gameObject.transform.position.x - centrePoint.x)) ;
+            }
+        }
+    }
+}
