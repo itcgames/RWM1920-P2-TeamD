f8ac47de7022c090075f551356958776456549c3
Dylan Curran <c00217112@itcarlow.ie>
2019-11-26 12:14:24 +0000

    committing parachute and base folder setup

---
+++ P1-Parachute-Dylan-Curran/README.md
@@ -0,0 +1,2 @@
+# P1-Parachute-Dylan-Curran
+Creating a parachute object for a puzzle game
---
+++ P1-Parachute-Dylan-Curran/p1Parachute2D/Assets/_scripts/GravityObjectScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityObjectScript : MonoBehaviour
+{
+	public GameObject objectFalling;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+		objectFalling.GetComponent<Rigidbody2D>().gravityScale = 0.1f;
+		gameObject.GetComponent<Rigidbody2D>().gravityScale = 0.1f;
+		
+	}
+}
