841722f981e1526565c58db5e5b8e1fb245a1a4b
Keith Wilson <c00205321@itcarlow.ie>
2019-11-27 12:00:29 +0000

    main menu and scene transitions implemented

---
+++ RWM1920-P2-TeamD/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.transform != null)
+            {
+                if(hit.transform.gameObject.tag == "NewGame")
+                {
+                    SceneManager.LoadScene(1);
+                }
+                if (hit.transform.gameObject.tag == "Options")
+                {
+                    SceneManager.LoadScene(2);
+                }
+                if (hit.transform.gameObject.tag == "Quit")
+                {
+                    Application.Quit();
+                    print("Derp");
+                }
+            }
+        }
+    }
+}
