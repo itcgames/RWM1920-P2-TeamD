05f8e9a5f5459883d9f42703173b686cb5b5e779
Martin <c00157047@itcarlow.ie>
2019-11-27 13:55:30 +0000

    updated ball script

--- RWM1920-P2-TeamD/Assets/Scripts/MartinScripts/BallBoundaryDetection.cs
+++ RWM1920-P2-TeamD/Assets/Scripts/MartinScripts/BallBoundaryDetection.cs
@@ -14,12 +14,12 @@ public class BallBoundaryDetection : MonoBehaviour
     {
         if(transform.position.y < -5.8)
         {
-            transform.position = new Vector3(transform.position.x, 4, 0);
+            transform.position = new Vector3(0, 4, 0);
             this.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
         }
         else if (transform.position.y > 5.8)
         {
-            transform.position = new Vector3(transform.position.x, 4, 0);
+            transform.position = new Vector3(0, 4, 0);
             this.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
         }
         else if (transform.position.x > 11)
