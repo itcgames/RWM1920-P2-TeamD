859801b3e679eb5734e843ba7ff8bb11bde42a0c
GregCahillC00216328 <gregcahill17@gmail.com>
2019-11-27 09:25:17 +0000

    Added greg grav comp.

---
+++ RWM1920-P2-TeamD/Assets/Scripts/InvertToggle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvertToggle : MonoBehaviour {
+
+    Vector2 centrePoint;
+    public GameObject Go;
+    GravityInvert grav;
+    
+
+    void Start()
+    {
+        centrePoint = GetComponent<Renderer>().bounds.center;
+       
+        grav = Go.GetComponent<GravityInvert>();
+        
+
+
+
+    }
+    
+    void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag==("Finish"))
+        {
+            //Debug.Log("col detected");
+            if (other.gameObject.transform.position.x <centrePoint.x)
+            {
+                GravityInvert.invertVal = (other.gameObject.transform.position.x - centrePoint.x) ;
+            }
+            else if (other.gameObject.transform.position.x > centrePoint.x)
+            {
+                GravityInvert.invertVal = ((other.gameObject.transform.position.x - centrePoint.x)) ;
+            }
+        }
+    }
+}
