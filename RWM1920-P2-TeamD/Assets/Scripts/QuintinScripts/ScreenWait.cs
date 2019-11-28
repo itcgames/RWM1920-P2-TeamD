f2e8f2bffff4e374067bd0342d02ac8046f9b306
Quintin Furlong <c00218453@itcarlow.ie>
2019-11-28 15:29:46 +0000

    Sceene movement

---
+++ RWM1920-P2-TeamD/Assets/Scripts/QuintinScripts/ScreenWait.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class ScreenWait : MonoBehaviour
+{
+    public float timeLimit;
+    float timer;
+    void Start()
+    {
+        timer = 0;
+    }
+    void Update()
+    {
+        timer  += Time.deltaTime;
+        if(timer > timeLimit)
+        {
+            SceneManager.LoadScene(3);//main menu
+        }
+    }
+}
