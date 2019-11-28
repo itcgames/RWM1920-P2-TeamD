dd1e6529938407344e9bcb13e9e32dc9ddcedd16
Martin <c00157047@itcarlow.ie>
2019-11-28 09:22:31 +0000

    no changes

--- RWM1920-P2-TeamD/Assets/Scripts/MartinScripts/BallBoundaryDetection.cs
+++ RWM1920-P2-TeamD/Assets/Scripts/MartinScripts/BallBoundaryDetection.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 
 public class BallBoundaryDetection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if(transform.position.y < -5.8)
@@ -32,8 +26,5 @@ public class BallBoundaryDetection : MonoBehaviour
             transform.position = new Vector3(0, 4, 0);
             this.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
         }
-
-
-
     }
 }
