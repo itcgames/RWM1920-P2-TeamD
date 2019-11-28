9c5e8b0d987535bbed37cc7fee38974845cfd4dd
GregCahillC00216328 <gregcahill17@gmail.com>
2019-11-28 15:40:09 +0000

    Added reset button prefab

---
+++ RWM1920-P2-TeamD/Assets/Scripts/GregScripts/Reset.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Reset : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            
+            if (hit.transform != null)
+            {
+                Debug.Log("Derp");
+                if (hit.transform.gameObject.tag == "ResetBT")
+                {
+                    Scene scene = SceneManager.GetActiveScene();
+                    SceneManager.LoadScene(scene.name);
+
+                }
+            }
+        }
+    }
+}
