676124a1296fc01379be1d2033155766a399308a
keithw96 <c00205321@itcarlow.ie>
2019-11-28 22:37:13 +0000

    levers refactored to work with multiple prefabs of the same object

--- RWM1920-P2-TeamD/Assets/Scripts/KeithScripts/LeverBehaviour.cs
+++ RWM1920-P2-TeamD/Assets/Scripts/KeithScripts/LeverBehaviour.cs
@@ -11,38 +11,27 @@ public class LeverBehaviour : MonoBehaviour
 
 	public AudioSource leverSource;
 	public AudioClip leverClip;
+
 	private void Start()
 	{
 		leverSource.clip = leverClip;
 	}
-	// Update is called once per frame
-	void Update()
+
+    private void OnMouseOver()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && this.tag == "PipeLever")
         {
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-
-            if (hit.transform != null)
-            {
-                if (hit.transform.tag == "PipeLever")
-                {
-					leverSource.Play();
-                    Debug.Log("pipe lever works");
-                    pipeScript.toggleActive();
-                }
-                else if (hit.transform.tag == "ConveyorLever")
-                {
-					leverSource.Play();
-					cog1Rotate.ToggleRight();
-                    cog2Rotate.ToggleRight();
-                    conveyorBelt.toggleEndpoint();
-                }
-            }
+            leverSource.Play();
+            Debug.Log("pipe lever works");
+            pipeScript.toggleActive();
         }
-    }
 
-    private void printName(GameObject go)
-    {
-        print(go.name);
+        if (Input.GetMouseButtonDown(0) && this.tag == "ConveyorLever")
+        {
+            leverSource.Play();
+            cog1Rotate.ToggleRight();
+            cog2Rotate.ToggleRight();
+            conveyorBelt.toggleEndpoint();
+        }
     }
 }
