38b672007bc1d25638d132e13c97ec7e575fadcd
GregCahillC00216328 <gregcahill17@gmail.com>
2020-01-22 15:26:46 +0000

    Keys added

---
+++ RWM1920-P2-TeamD/Assets/Scripts/GregScripts/Key.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    public bool Collected = false;
+    private bool switchDir = false;
+    int speed = 5;
+    void Update()
+    {
+        if(gameObject.transform.rotation.eulerAngles.z>5)
+        {
+            gameObject.transform.rotation.Set(0, 0, 4,0);
+            speed *= -1;
+        }
+        if (gameObject.transform.rotation.eulerAngles.z < -5)
+        {
+            gameObject.transform.rotation.Set(0, 0, -4, 0);
+            speed *= -1;
+        }
+        transform.Rotate(Vector3.forward * speed * Time.deltaTime);
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && Collected == false)
+        {
+            Collected = true;
+            SpriteRenderer spriteR;
+            spriteR= GetComponent<SpriteRenderer>();
+            spriteR.sprite = null;
+        }
+    }
+    public bool getCollected()
+    {
+        return Collected;
+    }
+}
